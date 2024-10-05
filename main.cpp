5ce52bb8aee62352644eee1f7570feffbb6f54a8
Kylie Roxo <kylieroxo@Kylies-MacBook-Pro.local>
2024-10-04 20:08:17 -0400

    first commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <vector>
 
+using namespace std;
+
 class Color{
     private:
     int red;
@@ -13,3 +15,24 @@ class Color{
     Color(int r, int b, int g){red = r; blue = b; green = g;}
 
 };
+
+int main(){
+    int r;
+    int b;
+    int g;
+    srand((unsigned) time(NULL));
+    int n = 25 + (rand() % 25);
+
+    Color color1;
+
+    for (int i = 0; i < n; i++){
+        r = 1 + (rand() % 999);
+    }
+
+    
+    cout << setw(15) << left << "R";
+    cout << setw(15) << left << "B";
+    cout << setw(15) << left << "G" << endl;
+
+    return 0;
+}
